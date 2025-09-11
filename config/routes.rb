bd9ea7f6de3e13f67f4c7f35dbc8fd61e93d8701
WataruKanzaki <224184043+zaki4615@users.noreply.github.com>
2025-09-11 01:44:48 +0000

    9章完了

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  
   devise_for :users
   root to: 'homes#top'
+
+  resources :post_images, only: [:new, :index, :show]
+  
   get 'homes/about', to: 'homes#about', as:'about'
   
 end
